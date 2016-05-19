52acd992fb2b47eecce00197d2ead51286f1666e
CarloCloudEmployee <carlo@cloudemployee.co.uk>
2016-05-19 22:17:47 +0800

    Refactored the JS scripts.

--- resources/js/src/index.js
+++ resources/js/src/index.js
@@ -1,39 +1,23 @@
 import $ from "./../vendor/jquery/dist/jquery";
 import {sum, square, MyClass} from "./import";
 import "./../vendor/mmenu.min";
+import slick from "./../vendor/slick/rdslick.js";
+import {navigation} from "./navigation";
+import {slider} from "./slider";
+import {footerLinkToggle} from "./footer";
 
 $(document).ready(init);
 
 function init(){
+	$.fn.slick = slick;
 	runImportedFunctions();
 }
 
 function runImportedFunctions(){
 	
-	const nav = $(".js-primary-nav");
-
-	nav.mmenu({
-		offCanvas: {
-			position: "left",
-			zposition: "front"
-		}
-	});
-
-	nav.find('.mm-navbar').append('<button class="primary-nav__mobile-btn-close js-primary-nav-btn-close">x</button>');
-
-	const navBtn = $(".header__nav--menu");
-	navBtn.on('click', function(){
-		nav.data('mmenu').open();
-	});
-
-	const navBtnBlose = $(".js-primary-nav-btn-close");
-	navBtnBlose.on('click', function(){
-		nav.data('mmenu').close();
-	});
-
-	$(".links-toggle").on("click",function() {
-		$(".footer__links").toggleClass("isHidden");
-	});
+	navigation();
+	slider();
+	footerLinkToggle();
 
 	$(".portlet-faq").on("click", function(e) {
 		$(".portlet-faq").removeClass("isActive");
